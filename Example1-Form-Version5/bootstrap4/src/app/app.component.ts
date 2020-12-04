1ea222e0f720a8dec7d14077f580dd4d8cc299df
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-12-03 19:51:05 -0500

    Added example 1v5

---
+++ Example1-Form-Version5/bootstrap4/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms"
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  defaultQuestion = 'teacher'
+  
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form)
+  }
+}
